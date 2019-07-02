b92d68fadb06d5e5a0ff8cd0b5a0b43d594b0d35
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2019-07-01 23:34:04 -0500

    Add files via upload

---
+++ proxy/proxy.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+#mock example use
+#terminal 1: nc -l -p 4200
+#terminal 2: ./proxy localhost 4200
+#terminal 3: nc localhost 1337 //then type 'a'+enter
+
+if [ $# != 2 ]
+then
+    printf "[usage]: $0 dest-addr dest-port\n"
+    exit 0
+fi
+
+#bash variable holding python cmd str
+read -r -d '' client_script <<-"EOF"
+#client packet parser script
+import struct
+import sys
+import os
+
+client=os.open(sys.argv[1], os.O_RDWR)
+packet=bytearray(os.read(client, 577))
+sys.stdout.write(str(packet)+'\n')
+EOF
+
+#bash variable holding python cmd str
+read -r -d '' server_script <<-"EOF"
+#server packet parser script
+import struct
+import sys
+import os
+
+client=os.open(sys.argv[1], os.O_RDWR)
+packet=bytearray(os.read(client, 577))
+sys.stdout.write(str(packet)+'\n')
+EOF
+
+#call relative packet parsing cmds & scripts
+client_parser(){
+    for (( ; ; ))
+    do
+	#timeout 0.1s od -A x -t x1z -v <$client
+	#timeout 0.1s hexdump -C -v <$client
+	python3 -c "$client_script" $client
+    done
+}
+
+#call relative packet parsing cmds & scripts
+server_parser(){
+    for (( ; ; ))
+    do
+	timeout 0.1s od -A x -t x1z -v <$server
+	#timeout 0.1s hexdump -C -v <$server
+	#python3 -c "$server_script" $server
+    done
+}
+
+#record all 127.0.0.1 traffic during uptime
+packet_capture(){
+    touch $tmp/packet_capture.pcap
+    tshark -i lo -r $tmp/packet_capture.pcap &
+}
+
+#proxy's client facing port
+proxy_port="1337"
+
+#make tmp directory and files for named pipes
+tmp=`mktemp -d`
+loop="$tmp/pipe.loop"
+client="$tmp/pipe.client"
+server="$tmp/pipe.server"
+
+#make named pipes
+mkfifo -m 0600 $loop $client $server
+
+#on exit signal run cmd str
+trap 'rm -rf $tmp/pipe.*;mv $tmp/packet_capture.pcap /tmp' EXIT
+
+                 #execute the
+client_parser &  #parse client traffic,
+server_parser &  #parsse server traffic,
+packet_capture & #record traffic
+                 #and, the 
+                 #proxy server
+nc -l -p $proxy_port <$loop | tee $client | nc $1 $2 | tee $server >$loop
