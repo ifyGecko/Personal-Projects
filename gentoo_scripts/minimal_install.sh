f2ca8ad037e7159a5443ce71e88e5433e6beffa6
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2019-12-14 02:11:48 -0600

    Update minimal_install.sh

--- gentoo_scripts/minimal_install.sh
+++ gentoo_scripts/minimal_install.sh
@@ -49,7 +49,7 @@ mount --make-rslave /mnt/gentoo/sys
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
 
-chroot /mnt/gentoo /bin/bash
+chroot /mnt/gentoo /bin/bash -x <<'EOF'
 source /etc/profile
 export PS1="(chroot) ${PS1}"
 
@@ -116,4 +116,4 @@ cd
 umount -l /mnt/gentoo/dev{/shm,/pts,}
 umount -R /mnt/gentoo
 reboot
-
+EOF
