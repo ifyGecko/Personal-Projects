5735ca51d2ee7e30d5b44946942c709fc514bd98
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2019-12-23 05:53:05 -0600

    Update freebsd.sh

--- system_scripts/freebsd.sh
+++ system_scripts/freebsd.sh
@@ -4,10 +4,8 @@ pkg install -y xorg
 
 pkg install -y stumpwm
 
-touch .xinitrc
 echo "startx /usr/share/bin/stumpwm" > .xinitrc
 
-touch .Xdefaults
 echo "XTerm*background:BLACK" > .Xdefaults
 echo "XTerm*foreground:RED" >> .Xdefaults
 
