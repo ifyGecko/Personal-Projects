76ef8ec2e2934d45d205406bbccb088819e92257
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2019-12-24 01:17:08 -0600

    Update gentoo.sh

--- system_scripts/gentoo.sh
+++ system_scripts/gentoo.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash 
 
+###############################################################
+###***ONLY USE GENTOO INSTALLATION MEDIA WITH THIS SCRIPT***###
+###############################################################
+
+# set-up disk partitions
 parted --script -a optimal -- /dev/sda \
        mklabel gpt \
        unit mib \
@@ -11,111 +16,137 @@ parted --script -a optimal -- /dev/sda \
        mkpart primary 300 -1 \
        name 3 rootfs
 
+# format new partitions
 mkfs.ext2 /dev/sda2
 mkfs.ext4 /dev/sda3
 
+# mount root partition
 mount /dev/sda3 /mnt/gentoo
 
+# set-up swapfile to use for swap space (instead of swap partition)
 dd if=/dev/zero of=/mnt/gentoo/swapfile bs=1MB count=2048
 chmod 600 /mnt/gentoo/swapfile
 mkswap /mnt/gentoo/swapfile
 swapon /mnt/gentoo/swapfile
 
+# network time protocal daemon
 ntpd -q -g
 
+# change directory to install stage3
 cd /mnt/gentoo
 
+# download stage3 archive
 links https://gentoo.org/downloads/
 
+# unarchive stage3
 tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 
+# set make parallel job count
 echo 'MAKEOPTS="-j4"' >> /mnt/gentoo/etc/portage/make.conf
 
+# select 3 fastest mirrors
 mirrorselect -s3 -b10 -D >> /mnt/gentoo/etc/portage/make.conf
 
+# configure ebuild repo
 mkdir --parents /mnt/gentoo/etc/portage/repos.conf
-
 cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
 
+# copy dns info
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 
+# mount necessary filesystems
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
 
+# chroot to /mnt/gentoo
 chroot /mnt/gentoo /bin/bash -x <<'EOF'
 source /etc/profile
 export PS1="(chroot) ${PS1}"
 
+# mount /boot partition
 mount /dev/sda2 /boot
 
+# grab latest portage snapshot
 emerge-webrsync
 emerge --sync
 
+# set hardened profile
 eselect profile set 18
 
+# update @world set
 emerge -vuDN @world
 
+# set timezone and locale
 echo "America/Chicago" > /etc/timezone
-
 emerge --config sys-libs/timezone-data
-
 echo "LANG="en_US.UTF-8"
 LC_COLLATE="C"" > /etc/env.d/02locale
 
+# update environment
 env-update && source /etc/profile && export PS1="(chroot) $PS1"
 
+# install kernel sources
 emerge sys-kernel/gentoo-sources
 
+### Need to figure out why --autounmask is needed ###
 emerge --autounmask-write sys-kernel/genkernel
 echo "-3" | etc-update
 emerge sys-kernel/genkernel
+#####################################################
 
+# create fstab
 echo "/dev/sda2      /boot  ext2   defaults,noatime     0 2" > /etc/fstab
-
-genkernel all
-
-emerge sys-kernel/linux-firmware
-
 echo "/dev/sda3      /      ext4   noatime       0 1" >> /etc/fstab
-
 echo "/swapfile      none   swap   sw,loop       0 0" >> /etc/fstab
 
+# auto generate and install linux kernel 
+genkernel all
+
+# install optional firmware
+emerge sys-kernel/linux-firmware
+
+# define a system hostname
 echo 'hostname="gentoo"' > /etc/conf.d/hostname
 
+# bring netif up on boot using dhcp
+echo 'config_eth0="dhcp"' > /etc/conf.d/net
 cd /etc/init.d 
-ln -s net.lo net.wlp3s0
-rc-update add net.wlp3s0 default
+ln -s net.lo net.enp3s0
+rc-update add net.enp3s0 default
 
+# system logging
 emerge app-admin/sysklogd
 rc-update add sysklogd default
 
-emerge sys-fs/e2fsprogs
-emerge sys-fs/dosfstools
-
+# install dhcp clinet daemon to obtain ip addr for nic(s)
 emerge net-misc/dhcpcd
-emerge net-wireless/iw net-wireless/wpa_supplicant
 
-emerge -v sys-boot/grub:2
+# install bootloader
+emerge sys-boot/grub:2
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
+# set default root password
 (echo "toor"; echo "toor") | passwd
 
+
+# optional (multiline comment, remove to use)
+: '
 cd ~/
-
-emerge app-editors/emacs
-
+emerge x11-base/xorg-server
 echo "XTerm*background:BLACK" > .Xdefaults
 echo "XTerm*foreground:RED" >> .Xdefaults
-
-emerge x11-base/xorg-server
+emerge app-editors/emacs
+emerge www-client/firefox
+'
 
 exit
 EOF
 
+# unmount chroot env and reboot
 cd
 umount -l /mnt/gentoo/dev{/shm,/pts,}
 umount -R /mnt/gentoo
